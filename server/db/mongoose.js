76273c8e3b4a1180447a4d186d6048a1e9839378
Julian Greculescu <julian.greculescu@gmail.com>
2017-03-16 09:24:02 +1100

    Add MONGODB_URL environment variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-if (process.env.PORT) {
-  mongoose.connect('mongodb://julian:nailuj@ds131510.mlab.com:31510/todo_app',
-    {server: {socketOptions: {connectTimeoutMS: 30000, socketTimeoutMS: 10000}}});
-} else {
-  mongoose.connect('mongodb://localhost:27017/todo_app');
-}
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+var dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/todo_app';
+mongoose.connect(dbUrl, {server: {socketOptions: {connectTimeoutMS: 30000, socketTimeoutMS: 10000}}});
